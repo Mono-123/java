413f885c2d65e5b90beeacda09b0287377da0bb2
user <user@user.com>
2023-05-23 00:03:24 +0800

    mybatis annotation

--- src/main/java/com/aliyun/demo/Main.java
+++ src/main/java/com/aliyun/demo/Main.java
@@ -1,20 +1,23 @@
 package com.aliyun.demo;
 
+import java.util.List;
 import java.util.Properties;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.InputStreamReader;
+import java.io.Reader;
 import com.aliyun.demo.Entity.Student;
+import com.aliyun.demo.Mapper.StudentMapper;
 
 public class Main {
     public static void main(String[] args) {
         Properties properties = new Properties();
         try {
             properties.load(
-                Main.class.getClassLoader().getResourceAsStream("config.properties")
-            );
+                    Main.class.getClassLoader().getResourceAsStream("config.properties"));
 
             // JDBC连接的URL, 不同数据库有不同的格式:
             String JDBC_URL = properties.getProperty("JDBC_URL");
@@ -26,126 +29,61 @@ public class Main {
             System.out.println(JDBC_USER);
             System.out.println(JDBC_PASSWORD);
 
-            try {
-                // Class.forName("com.mysql.cj.jdbc.Driver");
-                // 获取连接:
-                Connection conn = DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
+            Reader reader = new InputStreamReader(
+                    Main.class.getClassLoader().getResourceAsStream("mybatis-config.xml"));
+            SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(reader, properties);
 
-                // printByName(conn, "明%\" AND gender = 0 OR name Like \"%");
-                // printByName2(conn, "%明%");
-                // printByName2(conn, "%明% OR name Like %%");
-                // add(conn, "Tom", 1, 1, 100);
-                // Student student = new Student();
-                // student.setName("Jerry");
-                // student.setGender(1);
-                // student.setGrade(1);
-                // student.setScore(99);
-                // add(conn, new Student("Jane", 1, 2, 98));
-                // 关闭连接:
-                conn.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            factory.getConfiguration().addMapper(StudentMapper.class);
+            SqlSession session = factory.openSession();
+            StudentMapper studentMapper = session.getMapper(StudentMapper.class);
+
+            System.out.println("printall");
+            printAll(studentMapper);
+            System.out.println("print id = 4");
+            printById(studentMapper, 4);
+            System.out.println("print id = 9999");
+            printById(studentMapper, 9999);
+
+            long id_new = studentMapper.insert(new Student("Alex", 0, 0, 0));
+            session.commit();
+            System.out.println("print after insert: " + id_new);
+
+
+            long id = 9;
+            Student student = studentMapper.getById(id);
+            System.out.println(student);
+            student.setScore(student.getScore() + 10);
+            System.out.println("add 10 for student: ");
+            System.out.println(student);
+            studentMapper.updateScore(student);
+            session.commit();
+            System.out.println("print after update: " + id);
+            printById(studentMapper, id);
+            studentMapper.deleteById(id);
+            session.commit();
+            System.out.println("print after delete: " + id);
+            printById(studentMapper, id);
+            
+            session.close();
         } catch (Exception e) {
             System.out.println("读取配置文件失败");
             e.printStackTrace();
         }
     }
 
-    public static void printByGender(Connection conn, int gender) {
-        try {
-            System.out.println("print gender is: " + gender);
-
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT id, grade, name, gender FROM students WHERE gender=" + gender);
-
-            while (rs.next()) {
-                long id = rs.getLong(1); // 注意：索引从1开始
-                long grade = rs.getLong(2);
-                String name = rs.getString(3);
-                int genderResult = rs.getInt(4);
-                System.out.println(id + "," + grade + name + genderResult);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+    public static void printAll(StudentMapper studentMapper) {
+        List<Student> students = studentMapper.getAll(10, 0);
+        for (Student stu : students) {
+            System.out.println(stu);
         }
     }
 
-    public static void printByName(Connection conn, String name) {
-        try {
-            System.out.println("print name like: " + name);
-
-            Statement stmt = conn.createStatement();
-            String sql = "SELECT id, grade, name, gender FROM students WHERE name LIKE \"%" + name + "%\"";
-            System.out.println("执行了这个SQL: "+ sql);
-            ResultSet rs = stmt.executeQuery(sql);
-
-            while (rs.next()) {
-                long id = rs.getLong(1); // 注意：索引从1开始
-                long grade = rs.getLong(2);
-                String nameResult = rs.getString(3);
-                int gender = rs.getInt(4);
-                System.out.println(id + "," + grade + nameResult + gender);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void printByName2(Connection conn, String name) {
-        try {
-            System.out.println("print name like: " + name);
-
-            String sql = "SELECT id, grade, name, gender FROM students WHERE name LIKE ?";
-            PreparedStatement ps = conn.prepareStatement(sql);
-            ps.setObject(1, name);
-            ResultSet rs = ps.executeQuery();
-
-            while (rs.next()) {
-                long id = rs.getLong(1); // 注意：索引从1开始
-                long grade = rs.getLong(2);
-                String nameResult = rs.getString(3);
-                int gender = rs.getInt(4);
-                System.out.println(id + "," + grade + nameResult + gender);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void add(Connection conn, String name, int grade, int gender, int score) {
-        try {
-            System.out.println("insert: " + name + grade + gender + score);
-
-            String sql = "INSERT INTO students (name, grade, gender, score) VALUES (?,?,?,?)";
-            PreparedStatement ps = conn.prepareStatement(sql);
-            ps.setObject(1, name);
-            ps.setObject(2, grade);
-            ps.setObject(3, gender);
-            ps.setObject(4, score);
-            int rows = ps.executeUpdate();
-
-            System.out.println("inserted: " + rows);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void add(Connection conn, Student student) {
-        try {
-            System.out.println("insert: " + student.toString());
-
-            String sql = "INSERT INTO students (name, grade, gender, score) VALUES (?,?,?,?)";
-            PreparedStatement ps = conn.prepareStatement(sql);
-            ps.setObject(1, student.getName());
-            ps.setObject(2, student.getGrade());
-            ps.setObject(3, student.getGender());
-            ps.setObject(4, student.getScore());
-            int rows = ps.executeUpdate();
-
-            System.out.println("inserted: " + rows);
-        } catch (Exception e) {
-            e.printStackTrace();
+    public static void printById(StudentMapper studentMapper, long id) {
+        Student student = studentMapper.getById(id);
+        if (student == null) {
+            System.out.println("id: " + id + " not found");
+        } else {
+            System.out.println(student);
         }
     }
 }
---
+++ src/main/java/com/aliyun/demo/Mapper/StudentMapper.java
@@ -0,0 +1,30 @@
+package com.aliyun.demo.Mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import com.aliyun.demo.Entity.Student;
+
+public interface StudentMapper {
+    @Select("SELECT * FROM students WHERE id = #{id}")
+	Student getById(@Param("id") long id);
+    
+    @Select("SELECT * FROM students LIMIT #{limit} OFFSET #{offset}")
+    List<Student> getAll(@Param("limit") int limit, @Param("offset") int offset);
+
+    @Insert("INSERT INTO students (name, gender, grade, score) VALUES (#{stu.name}, #{stu.gender}, #{stu.grade}, #{stu.score})")
+    @Options(useGeneratedKeys = true, keyProperty = "id", keyColumn = "id")
+    long insert(@Param("stu") Student student);
+
+    @Update("UPDATE students SET score = #{stu.score} WHERE id = #{stu.id}")
+    void updateScore(@Param("stu") Student student);
+
+    @Delete("DELETE FROM students WHERE id = #{id}")
+    void deleteById(@Param("id") long id);
+}
