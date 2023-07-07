e4cb7b01c34969fe0874f471ab1030299fa2b0a9
Mono-123 <1321807420@qq.com>
2023-07-07 18:07:20 +0800

    add upload

--- src/main/java/com/aliyun/learnjava/controller/StudentController.java
+++ src/main/java/com/aliyun/learnjava/controller/StudentController.java
@@ -1,5 +1,6 @@
 package com.aliyun.learnjava.controller;
 
+import java.io.File;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,8 +14,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
 
 import com.aliyun.learnjava.service.StudentService;
+
 import com.aliyun.exception.NotFoundException;
 import com.aliyun.learnjava.entity.Student;
 
@@ -23,6 +27,50 @@ import com.aliyun.learnjava.entity.Student;
 public class StudentController {
     @Autowired
     private StudentService studentService;
+    // // 上传路径
+    // @Value("${document.uploadPath}")
+    // private String uploadPath;
+
+    // // 显示路径
+    // @Value("${document.imgPath}")
+    // private String imgPath;
+
+// @RequestMapping("/upload")
+// public class UploadController {
+ 
+//     @PostMapping
+//     public String upload(@RequestParam("file") MultipartFile file, HttpServletRequest request) throws Exception {
+//         // file:上传文件
+//         // 获取到 images 的具体路径
+//         // String realPath = request.getRealPath("images");
+//         String realPath = ResourceUtils.getURL("classpath:").getPath() + "/static/images";
+//         System.out.println("上传的文件地址是：" + realPath);
+//         // 服务器中对应的位置
+//         // 产生唯一的文件名称
+//         String fileName = UUID.getUUid();
+//         // 获取到文件后缀
+//         String fileType = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf("."));
+//         File src = new File(realPath, fileName + fileType);
+//         // 将file文件传递到src去
+//         file.transferTo(src);
+//         return "images/" + fileName + fileType;
+//     }
+// }
+
+
+    @PostMapping("/upload")
+    public String upload(@RequestParam("file") MultipartFile file, @RequestParam("name") String name)
+            throws Exception {
+        // 设置上传至项目文件夹下的uploadFile文件夹中，没有文件夹则创建
+        System.out.println("upload: " + file);
+        File dir = new File("uploadFile");
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        file.transferTo(new File(dir.getAbsolutePath() + File.separator + name + ".png"));
+        return "上传完成！文件名：" + name;
+    }
+
 
     @GetMapping("/list")
     public List<Student> getAll(
@@ -30,7 +78,7 @@ public class StudentController {
             @RequestParam(name = "desc", required = false, defaultValue = "0") int desc,
             @RequestParam(name = "limit", required = false, defaultValue = "10") int limit,
             @RequestParam(name = "offset", required = false, defaultValue = "0") int offset) {
-        List<Student> students = this.studentService.getAll(order,desc, limit, offset);
+        List<Student> students = this.studentService.getAll(order, desc, limit, offset);
         return students;
     }
 
@@ -45,12 +93,12 @@ public class StudentController {
 
     @GetMapping("/condition")
     public List<Student> getCondition(
-            @RequestParam(name = "name" ,required = false) String name,
-            @RequestParam(name = "gender" ,required = false) String gender,
-            @RequestParam(name = "grade" ,required = false) String grade,
-            @RequestParam(name = "score" ,required = false) String score) throws NotFoundException {
+            @RequestParam(name = "name", required = false) String name,
+            @RequestParam(name = "gender", required = false) String gender,
+            @RequestParam(name = "grade", required = false) String grade,
+            @RequestParam(name = "score", required = false) String score) throws NotFoundException {
         System.out.println("name: " + name + "gender: " + gender + "grade: " + grade + "score: " + score);
-        List<Student> students = this.studentService.getCondition(name, gender,grade,score);
+        List<Student> students = this.studentService.getCondition(name, gender, grade, score);
         return students;
     }
 
